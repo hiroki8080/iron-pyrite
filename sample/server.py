24688f01d29ad8c3f19149cc7815f5695bcb7ea8
Murashige <hiroki.mura23@gmail.com>
2024-04-28 11:52:04 +0900

    Add py2wasm sample.

---
+++ sample/server.py
@@ -0,0 +1,24 @@
+import http.server
+import socketserver
+
+# Convert wasm module.
+#  py2wasm sample.py -o sample.wasm
+# Running the server.
+#  python server.py
+#
+
+PORT = 8000
+
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+Handler = HTTPRequestHandler
+Handler.extensions_map = {
+    '.wasm': 'application/wasm',
+}
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
